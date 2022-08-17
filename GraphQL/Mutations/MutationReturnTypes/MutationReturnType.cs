cc62f4fed3da42099b8f82f03718dcc0794449f6
NATHAN <NATHAN@DESKTOP-SQI81TB>
2022-08-17 11:25:39 +0200

    fix mutationReturnType

--- GraphQL/Mutations/MutationReturnTypes/MutationReturnType.cs
+++ GraphQL/Mutations/MutationReturnTypes/MutationReturnType.cs
@@ -3,7 +3,7 @@
     public class MutationReturnType
     {
         public int Code { get; set; }
-        public string Label { get; set; }
+        public string Label { get; set; } = "";
         public string Domain { get; set; } = "";
         public string Description { get; set; } = "";
     }
